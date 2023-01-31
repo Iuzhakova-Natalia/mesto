5cdfce96c730dbdf30d250ac5f54645b4574544f
Iuzhakova Natalia <nata-zhena@yandex.ru>
2023-01-31 15:06:59 +0300

    сверстать функционал

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ const editButton = document.querySelector('.profile__button_type_edit');
 const popup = document.querySelector('.popup');
 const closeBatton = popup.querySelector('.popup__close');
 
-
 const handleEditButtonClick = () => {
   popup.classList.add('popup_opened');  
 }
@@ -13,3 +12,26 @@ const handleCloseButtonClick = () => {
 
 editButton.addEventListener('click', handleEditButtonClick);
 closeBatton.addEventListener('click', handleCloseButtonClick);
+
+
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__input-text_type_name').value;
+let jobInput = document.querySelector('.form__input-text_type_job').value;
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+
+  nameInput = document.querySelector('.form__input-text_type_name').value;
+  jobInput = document.querySelector('.form__input-text_type_job').value;
+
+  let profileName = document.querySelector('.profile__name');
+  let profileJob = document.querySelector('.profile__job');
+
+  profileName.textContent = nameInput;
+  profileJob.textContent = jobInput;
+
+  handleCloseButtonClick();
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
