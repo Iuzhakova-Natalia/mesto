d1ec41656e3d8ea9963df90e44ce4c73e00ee5c7
Iuzhakova Natalia <nata-zhena@yandex.ru>
2023-04-05 10:23:01 +0300

    добавила пресеты css

--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          }
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
         ]
-      }
+      },
     ]
   },
   plugins: [
